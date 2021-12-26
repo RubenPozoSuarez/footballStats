b4d2c4dd1f21e1cadfe2e523aca704bcd0c7cad6
RubenPozoSuarez <rubenpozosuarez@gmail.com>
2021-12-26 23:52:30 +0100

    proyecto creado

---
+++ src/main/java/com/footballStats/Luian/LuianApplication.java
@@ -0,0 +1,13 @@
+package com.footballStats.Luian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/footballStats/Luian/LuianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footballStats.Luian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
